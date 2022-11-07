3214eb29f3c711b2becff24a76881415d6286fee
maxnesri <103585318+maxnesri@users.noreply.github.com>
2022-11-07 12:22:57 +0300

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,6 @@
+print("Задание 1")
+name = str(input("Введите имя: "))
+old = int(input("Введите ваш возраст: "))
+year = int(input("Введите год: "))
+new_old = year - 2022 + old
+print("Привет, ", name,"! В ", year, "году тебе будет", new_old)
